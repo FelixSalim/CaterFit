5528485c04906a27eeb7bb204fc0122d19ab0b6b
Felix Salim <felixsalim64@gmail.com>
2025-05-28 20:17:12 +0700

    First Commit

---
+++ lib/admin/package_management.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PackageManagement extends StatelessWidget {
+  const PackageManagement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:caterfit/register.dart';
+import 'package:caterfit/admin/package_management.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PackageManagement()));
+              },
+              child: const Text('Login')),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Register()));
+              },
+              child: const Text("Don't have account? Register")),
+        ],
+      ),
+    );
+  }
+}
