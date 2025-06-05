35d552b140787ef22df5d7b46408db624d641c58
Felix Salim <felixsalim64@gmail.com>
2025-06-05 12:34:22 +0700

    finished management page

--- lib/admin/package_management.dart
+++ lib/admin/package_management.dart
@@ -8,13 +8,40 @@ class PackageManagement extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          PackageManagementTitle(),
-          OnGoingPackage(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PackageManagementTitle(),
+            OnGoingPackage(),
+            ArchivedPackage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SubTitle extends StatelessWidget {
+  final String text;
+
+  const SubTitle({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Text(
+        text,
+        style: GoogleFonts.montserrat(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: Colors.black,
+        ),
       ),
     );
   }
@@ -47,31 +74,12 @@ class OnGoingPackage extends StatelessWidget {
     return const Column(
       children: [
         Row(
-          children: [OnGoingPackageTitle()],
+          children: [SubTitle(text: "On Going Package")],
         ),
         Row(
           children: [OnGoingPackageCards()],
         ),
       ],
-    );
-  }
-}
-
-class OnGoingPackageTitle extends StatelessWidget {
-  const OnGoingPackageTitle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Text(
-        "On Going Package",
-        style: GoogleFonts.montserrat(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          color: Colors.black,
-        ),
-      ),
     );
   }
 }
@@ -119,21 +127,292 @@ class PackageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(40.0)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: -3,
-                blurRadius: 4,
-                offset: const Offset(4, 4), // changes position of shadow
-              )
-            ]),
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Card(
-          child: Image.asset('Assets/$image', fit: BoxFit.cover),
+      child: Stack(
+        children: [
+          PackageCardFront(title: title, image: image),
+          PackageCardBack(title: title, image: image),
+        ],
+      ),
+    );
+  }
+}
+
+class PackageCardFront extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const PackageCardFront({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 200,
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(40.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: -3,
+              blurRadius: 4,
+              offset: const Offset(4, 4), // changes position of shadow
+            )
+          ]),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      margin: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Image.asset('Assets/$image', fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class PackageCardBack extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const PackageCardBack({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 200,
+      margin: const EdgeInsets.all(8.0),
+      decoration: const BoxDecoration(
+        color: Color(0xBF0D3011),
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    title,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.clip,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Container(
+                  width: 30,
+                  height: 30,
+                  margin: const EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0D3011),
+                    borderRadius: BorderRadius.circular(12.5),
+                  ),
+                  child: const Icon(Icons.edit, color: Colors.white, size: 24),
+                ),
+                Container(
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0D3011),
+                    borderRadius: BorderRadius.circular(12.5),
+                  ),
+                  child:
+                      const Icon(Icons.archive, color: Colors.white, size: 24),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ArchivedPackage extends StatelessWidget {
+  const ArchivedPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: [SubTitle(text: "Archived Package")],
         ),
+        Row(
+          children: [ArchivedPackageCards()],
+        ),
+      ],
+    );
+  }
+}
+
+class ArchivedPackageCards extends StatelessWidget {
+  const ArchivedPackageCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: const Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ArchiveCard(
+                  title: "School Meal Package", image: "PackageImageTemp.png"),
+              ArchiveCard(
+                  title: "School Meal Package", image: "PackageImageTemp.png"),
+            ],
+          )
+        ]));
+  }
+}
+
+class ArchiveCard extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const ArchiveCard({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          ArchiveCardFront(title: title, image: image),
+          ArchiveCardBack(title: title, image: image),
+        ],
+      ),
+    );
+  }
+}
+
+class ArchiveCardFront extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const ArchiveCardFront({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 200,
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(40.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: -3,
+              blurRadius: 4,
+              offset: const Offset(4, 4), // changes position of shadow
+            )
+          ]),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      margin: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Image.asset('Assets/$image', fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class ArchiveCardBack extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const ArchiveCardBack({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 200,
+      margin: const EdgeInsets.all(8.0),
+      decoration: const BoxDecoration(
+        color: Color(0xBF0D3011),
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    title,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.clip,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Container(
+                  width: 30,
+                  height: 30,
+                  margin: const EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0D3011),
+                    borderRadius: BorderRadius.circular(12.5),
+                  ),
+                  child: const Icon(Icons.undo, color: Colors.white, size: 24),
+                ),
+                Container(
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0D3011),
+                    borderRadius: BorderRadius.circular(12.5),
+                  ),
+                  child:
+                      const Icon(Icons.delete, color: Colors.white, size: 24),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
