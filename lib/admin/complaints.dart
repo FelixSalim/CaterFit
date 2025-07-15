0db5d6c48f856410ac38911be23f6275ed6c0e29
CalvinSentosa <calvinsentosa21@gmail.com>
2025-07-15 10:26:18 +0700

    Update Admin Complaints Page

--- lib/admin/complaints.dart
+++ lib/admin/complaints.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const ChatApp());
-}
+// void main() {
+//   runApp(const ChatApp());
+// }
 
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
