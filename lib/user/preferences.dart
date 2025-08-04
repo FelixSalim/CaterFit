464176385031d8420663d0ca85a86e87f2910d3f
Felix Salim <felixsalim64@gmail.com>
2025-08-04 22:31:34 +0700

    fixed navigation

--- lib/user/preferences.dart
+++ lib/user/preferences.dart
@@ -100,10 +100,7 @@ class _PreferencesPageState extends State<PreferencesPage> {
                   InkWell(
                     onTap: () {
                       // Navigasi ke ProfilePage
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const ProfilePage()),
-                      );
+                      Navigator.pop(context);
                     },
                     // Memberi area sentuh yang cukup
                     child: Padding(
@@ -129,7 +126,8 @@ class _PreferencesPageState extends State<PreferencesPage> {
                     ),
                   ),
                   // Spacer untuk menyeimbangkan tombol kembali agar judul tetap di tengah
-                  const SizedBox(width: 48), // Disesuaikan dengan padding InkWell
+                  const SizedBox(
+                      width: 48), // Disesuaikan dengan padding InkWell
                 ],
               ),
               // REVISI: Mengurangi jarak dari header
@@ -147,7 +145,8 @@ class _PreferencesPageState extends State<PreferencesPage> {
                   child: Text(
                     "You haven't selected any preferences",
                     textAlign: TextAlign.left,
-                    style: GoogleFonts.nunitoSans( // Diperbarui sesuai permintaan Anda
+                    style: GoogleFonts.nunitoSans(
+                      // Diperbarui sesuai permintaan Anda
                       color: Colors.red[800],
                       fontSize: 14,
                       fontWeight: FontWeight.w500,
